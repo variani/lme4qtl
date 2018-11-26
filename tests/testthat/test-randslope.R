46c3e5819214f234587f25461410ed1a3f61b518
variani <machovariani@gmail.com>
2018-11-26 13:21:32 -0500

    needed 2 formula + 2 IDs + 2 mat to specify a random slope model like relmatLmer(trait1 ~ AGE + (1|ID) + (0 + AGE|ID2), dat40, relmat = list(ID = kin2, ID2 = kin2)); related to issue #13

--- tests/testthat/test-randslope.R
+++ tests/testthat/test-randslope.R
@@ -1,6 +1,6 @@
 context("Random slope")
 
-test_that("cont. slope", {
+test_that("cont. slope; one ranef", {
   stopifnot(require(Matrix))
     
   data(dat40)
@@ -23,6 +23,66 @@ test_that("cont. slope", {
   u2_hat_fam <- unique(u2_hat)
 
   expect_equal(u1_hat, u2_hat_fam)  
+})
+
+test_that("cont. slope; 2 ranef + 2 formula terms", {
+  stopifnot(require(Matrix))
+    
+  data(dat40)
+  dat40 <- subset(dat40, FAMID %in% 1:5) # a smaller subset
+  dat40 <- within(dat40, ID2 <- ID)
+  
+  # model: random slope on FAMID; fitted by lme4  
+  m1 <- lmer(trait1 ~ AGE + (1 + AGE||FAMID), dat40)
+
+  # model: random slope on FAMID; fitted by lme4qtk using ID grouping and rel. matrix
+  # two models are equvalent (m1 and m2)  
+  mat_famid <- model.matrix(~ -1 + FAMID, dat40) %>% Matrix %>% tcrossprod
+  rownames(mat_famid) <- dat40$ID
+  colnames(mat_famid) <- dat40$ID
+
+  m2 <- relmatLmer(trait1 ~ AGE + (1|ID) + (0 + AGE|ID2), dat40, relmat = list(ID = mat_famid, ID2 = mat_famid))
+  
+  # get BLUP estimates 
+  u1_hat <- ranef(m1, which = "FAMID")[[1]][["AGE"]] %>% round(5)
+  u2_hat <- relmatRanef(m2, "ID2") %>% round(5)
+  u2_hat_fam <- unique(u2_hat)
+
+  expect_equal(u1_hat, u2_hat_fam)  
+})
+
+test_that("cont. slope; 2 ranef + 1 formula term", {
+  stopifnot(require(Matrix))
+    
+  data(dat40)
+  dat40 <- subset(dat40, FAMID %in% 1:5) # a smaller subset
+  dat40 <- within(dat40, ID2 <- ID)
+  
+  # model: random slope on FAMID; fitted by lme4  
+  m1 <- lmer(trait1 ~ AGE + (1 + AGE||FAMID), dat40)
+
+  # model: random slope on FAMID; fitted by lme4qtk using ID grouping and rel. matrix
+  # two models are equvalent (m1 and m2)  
+  mat_famid <- model.matrix(~ -1 + FAMID, dat40) %>% Matrix %>% tcrossprod
+  rownames(mat_famid) <- dat40$ID
+  colnames(mat_famid) <- dat40$ID
+
+  m2 <- relmatLmer(trait1 ~ AGE + (1|ID) + (0 + AGE|ID2), dat40, relmat = list(ID = mat_famid, ID2 = mat_famid))
+  
+  m3 <- relmatLmer(trait1 ~ AGE + (1 + AGE||ID), dat40, relmat = list(ID = mat_famid))
+  m4 <- relmatLmer(trait1 ~ AGE + (1|ID) + (0 + AGE|ID2), dat40, relmat = list(ID = mat_famid))
+
+  # get BLUP estimates 
+  u1_hat <- ranef(m1, which = "FAMID")[[1]][["AGE"]] %>% round(5)
+  u2_hat <- relmatRanef(m2, "ID2") %>% round(5)
+  u2_hat_fam <- unique(u2_hat)
+
+  u3_hat <- ranef(m3, which = "ID")[[1]][["AGE"]] %>% round(5)
+  u3_hat_fam <- unique(u3_hat)
+  u4_hat <- relmatRanef(m4, "ID2") %>% round(5)
+  u4_hat_fam <- unique(u4_hat)
+  
+  expect_equal(u3_hat, u4_hat_fam)  
 })
 
 test_that("GxE in dat30", {
