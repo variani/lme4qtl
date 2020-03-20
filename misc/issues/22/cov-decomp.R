abb82ce12eacb14719aa4f13573941974be1be8c
Andrey Ziyatdinov <machovariani@gmail.com>
2020-03-20 12:23:17 -0400

    example low-rank covariant #22

--- misc/issues/22/cov-decomp.R
+++ misc/issues/22/cov-decomp.R
@@ -1,5 +1,5 @@
 data(dat40, package = "lme4qtl")
-K <- kin2[1:5, 1:5]
+K <- as.matrix(kin2[1:5, 1:5])
 
 # Covariance matrix K and its decompoisiton K = L L',
 #   where L is lower triangular  matrix
@@ -28,3 +28,32 @@ R <- tcrossprod(sqrt(D), V) # D^{0.5} V'
 try(stopifnot(all(crossprod(R) == K)))
 stopifnot(all(round(crossprod(R), 10) == K))
 
+## 3. Example of low-rank cov. matrix Klr
+# see also lme4qtl:::relfac.evd
+Klr <- K
+Klr[3:5, 3:5] <- 1
+
+# dimension vs. rank (5 vs. 3)
+dim(Klr)
+qr(Klr)$rank
+
+# try Cholesky on Klr
+try(chol(Klr))
+
+# EVD on Klr + filter out zero eigen-values
+evd <- eigen(Klr, symmetric = TRUE)
+
+# filtering
+ind_zero <- (abs(evd$values) < 1e-10) # near-zero eigen values can be negative
+ev <- evd$values
+ev[ind_zero] <- 0
+
+# decomposition
+D <- diag(ev)
+V <- evd$vectors
+
+R <- tcrossprod(sqrt(D), V) # D^{0.5} V'
+
+# check
+try(stopifnot(all(crossprod(R) == Klr)))
+stopifnot(all(round(crossprod(R), 10) == Klr))
